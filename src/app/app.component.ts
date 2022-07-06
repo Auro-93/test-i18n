b6c74c780524bb1b4beb5f468e22c59416b6386a
Auro-93 <93a.sirigu@gmail.com>
2022-07-06 08:16:27 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test-i18n';
+export class AppComponent implements OnInit {
+
+  siteLanguage: string = 'English';
+  siteLocale: string;
+  languageList = [
+    { code: 'en', label: 'English' },
+    { code: 'it', label: 'Italian' }
+  ];
+  constructor() { }
+  ngOnInit() {
+    this.siteLocale = window.location.pathname.split('/')[1];
+  this.siteLanguage = this.languageList.find(f => f.code === this.siteLocale).label;
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule}  from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
